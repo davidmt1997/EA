de18f29bfb7add09032ee020f266a4d8c93d47f1
davidmt1997 <davidmt1997@gmail.com>
2020-11-21 12:37:00 +0100

    Add files via upload

--- neo_push.py
+++ neo_push.py
@@ -7,7 +7,7 @@ from facebook_scrapper import get_fb_info
 import logging
 from selenium_driver import Selenium_driver
 
-graph = Graph(password="")
+graph = Graph(password="03111997")
 
 # Steam is Fifa 21, can't find language for fifa 20
 FIFA_URLS = ["https://en.wikipedia.org/wiki/FIFA_20", "https://es.wikipedia.org/wiki/FIFA_20", "https://store.steampowered.com/app/1313860/EA_SPORTS_FIFA_21/", "https://www.facebook.com/easportsfifa", "EA"]
@@ -20,19 +20,19 @@ tx = graph.begin()
 def create_language_relationships_from_df(df, node):
 	for i in df.itertuples():
 		language = Node("Language", name=i[0])
-		rel = Relationship(node, "in", language)
+		rel = Relationship(node, "IN", language)
 		tx.create(rel)
 		if i[1] == 1:
 			interface_node = Node("Interface", name="Interface")
-			rel = Relationship(language, "has", interface_node)
+			rel = Relationship(language, "HAS", interface_node)
 			tx.create(rel)
 		if i[2] == 1:
 			audio_node = Node("Audio", name="Full Audio")
-			rel = Relationship(language, "has", audio_node)
+			rel = Relationship(language, "HAS", audio_node)
 			tx.create(rel)
 		if i[3] == 1:
 			subtitle_node = Node("Subtitles", name="Subtitle")
-			rel = Relationship(language, "has", subtitle_node)
+			rel = Relationship(language, "HAS", subtitle_node)
 			tx.create(rel)
 
 def create_commentary_relationships(df, node):
@@ -58,42 +58,71 @@ def create_competiciones_relationships(df, node):
 	competicion = df.iloc[0,0]
 	competicion1 = df.Competicion.unique()[0]
 	competicion2 = df.Competicion.unique()[1]
-	competicion_node1 = Node("Competicion", name=competicion1)
-	competicion_node2 = Node("Competicion", name=competicion2)
+	competicion_node1 = Node("Competiciones", name=competicion1)
+	competicion_node2 = Node("Competiciones", name=competicion2)
 	for i in df.itertuples():
 		if i[1] == competicion:
-			rel = Relationship(node, "competicion", competicion_node1)
+			rel = Relationship(node, "competiciones", competicion_node1)
 			tx.create(rel)
-			club_node = Node("Club", name=i[3])
-			rel = Relationship(competicion_node1, i[2], club_node)
+			country_node = Node("Country", name=i[2])
+			rel = Relationship(competicion_node1, "country", country_node)
+			tx.create(rel)
+			item_node = Node("Item", name=i[3])
+			rel = Relationship(country_node, "item", item_node)
 			tx.create(rel)
 			licencia_node = Node("Licencia", name=i[4])
-			rel = Relationship(club_node, "licencia", licencia_node)
+			rel = Relationship(item_node, "licenciada", licencia_node)
 			tx.create(rel)
 		else:
-			rel = Relationship(node, "competicion", competicion_node2)
+			rel = Relationship(node, "competiciones", competicion_node2)
 			tx.create(rel)
-			club_node = Node("Club", name=i[3])
-			rel = Relationship(competicion_node2, i[2], club_node)
+			country_node = Node("Country", name=i[2])
+			rel = Relationship(competicion_node2, "country", country_node)
+			tx.create(rel)
+			item_node = Node("Item", name=i[3])
+			rel = Relationship(country_node, "item", item_node)
 			tx.create(rel)
 			licencia_node = Node("Licencia", name=i[4])
-			rel = Relationship(club_node, "licencia", licencia_node)
+			rel = Relationship(item_node, "licenciada", licencia_node)
+			tx.create(rel)
+
+def create_pes_competiciones_relationships(df, node):
+	for i in df.itertuples():
+		if i[3] != "" and i[1] != "":
+			country_node = Node("Country", name=i[1])
+			rel = Relationship(node, "country", country_node)
+			tx.create(rel)
+			item_node = Node("Item", name=i[2])
+			rel = Relationship(country_node, "item", item_node)
+			tx.create(rel)
+			name = ""
+			if i[3] == "1":
+				name = "Sí"
+			else:
+				name = "No"
+			licencia_node = Node("Licencia", name=name)
+			rel = Relationship(item_node, "licenciada", licencia_node)
 			tx.create(rel)
 	
 
-def create_pes_competiciones(df, node):
+def create_pes_selecciones_relationships(df, node):
 	for i in df.itertuples():
 		if i[3] != "":
-			region_node = Node("Region", name=i[1])
-			rel = Relationship(node, "in", region_node)
+			region_node = Node("Country", name=i[2])
+			rel = Relationship(node, "country", region_node)
 			tx.create(rel)
-		
-			licencia_node = Node("#Licencia", name=i[3])
-			rel = Relationship(region_node, i[2], licencia_node)
-			tx.create(rel)
+			if i[1] != "":
+				name = ""
+				if i[3] == "1":
+					name = "Sí"
+				else:
+					name = "No"
+				licencia_node = Node("Licencia", name=name)
+				rel = Relationship(region_node, "licenciada", licencia_node)
+				tx.create(rel)
 
 def main():
-	for urls in [FIFA_URLS, PES_URLS]:
+	for urls in [FIFA_URLS, PES_URLS, NHL_URLS, NFL_URLS]:
 		for url in urls:
 
 			# If https in url, retrieving data using Selenium
@@ -110,20 +139,20 @@ def main():
 
 					print("Getting basic info table")
 					info = get_info_table(driver)
-					info_node = Node("Info", name="basic_info")
+					info_node = Node("Infos", name="basic_info")
 
 					print("Getting scores table")
 					scores = get_scores_table(driver)
 					scores_node = Node("Scores", name="scores")
 					# Insert game, info and scores into neo4j
 					
-					r1 = Relationship(game_node, "INFO", info_node)
+					r1 = Relationship(game_node, "HAS_INFO", info_node)
 					tx.create(r1)
-					r2 = Relationship(game_node, "has_scores", scores_node)
+					r2 = Relationship(game_node, "HAS_SCORES", scores_node)
 					tx.create(r2)
 
 					for x, y in info.items():
-						i = Node("Infos", name=y)
+						i = Node("Info", name=y)
 						rel = Relationship(info_node, x, i)
 						tx.create(rel)
 
@@ -139,15 +168,16 @@ def main():
 					commentaries = get_commentaries(driver)
 					commentary_node = Node("Commentaries", name="commentaries")
 					
-					rel = Relationship(game_node, "has_commentaries", commentary_node)
+					rel = Relationship(game_node, "HAS_COMMENTARIES", commentary_node)
 					tx.create(rel)
 					create_commentary_relationships(commentaries, commentary_node)
 					print("Getting licenses tables")
+					license_node = Node("Licencias", name="Licencias")
+					rel = Relationship(game_node, "licenses", license_node)
+					tx.create(rel)
 					if "fifa" in url.lower():
 						competiciones, selec_masc, selec_fem = get_fifa_licenses_table(driver)
-						license_node = Node("Licencias", name="Licencias")
-						rel = Relationship(game_node, "licenses", license_node)
-						tx.create(rel)
+						
 						selec_masc_node = Node("Selecciones", name="selecciones masculinas")
 						selec_fem_node = Node("Selecciones", name="selecciones femeninas")
 						rel = Relationship(license_node, "selecciones", selec_masc_node)
@@ -159,32 +189,29 @@ def main():
 						create_competiciones_relationships(competiciones, license_node)
 					else:
 						competiciones, selecciones = get_pes_licenses_table(driver)
-						license_node = Node("Licencias", name="Licencias")
-						rel = Relationship(game_node, "licenses", license_node)
-						tx.create(rel)
 						competicion_node = Node("Competiciones", name="competiciones")
 						rel = Relationship(license_node, "competiciones", competicion_node)
 						tx.create(rel)
-						seleccion_node = Node("Selcciones", name="selecciones")
+						seleccion_node = Node("Selecciones", name="selecciones")
 						rel = Relationship(license_node, "selecciones", seleccion_node)
 						tx.create(rel)
-						create_pes_competiciones(competiciones, competicion_node)
-						create_pes_competiciones(selecciones, seleccion_node)
+						create_pes_competiciones_relationships(competiciones, competicion_node)
+						create_pes_selecciones_relationships(selecciones, seleccion_node)
 				
 				# Getting steam data
 				if "steampowered" in url:
 					print("Getting languages table")
 					languages = get_languages(driver)
-					language_node = Node("Language", name="languages")
+					language_node = Node("Languages", name="languages")
 					
-					rel = Relationship(game_node, "has_languages", language_node)
+					rel = Relationship(game_node, "HAS_LANGUAGES", language_node)
 					tx.create(rel)
 					create_language_relationships_from_df(languages, language_node)
 					
 					print("Getting release date")
 					release_date = get_realease_date(driver)
 					release_node = Node("Date", name=release_date)
-					r = Relationship(game_node, "released", release_node)
+					r = Relationship(game_node, "RELEASED", release_node)
 					tx.create(r)
 					
 				
@@ -192,16 +219,16 @@ def main():
 				elif "facebook" in url:
 					print("Getting facebook data")
 					facebook_node = Node("SM", name="facebook")
-					rel = Relationship(game_node, "facebook_data", facebook_node)
+					rel = Relationship(game_node, "FB_DATA", facebook_node)
 					tx.create(rel)
 					likes, followers = get_fb_info(driver)
 					if likes != "":
 						fb_likes_node = Node("Likes", name=likes)
-						r3 = Relationship(facebook_node, "has_likes", fb_likes_node)
+						r3 = Relationship(facebook_node, "HAS_LIKES", fb_likes_node)
 						tx.create(r3)
 					if followers != "":
 						fb_followers_node = Node("Followers", name=followers)
-						r4 = Relationship(facebook_node, "has_followers", fb_followers_node)
+						r4 = Relationship(facebook_node, "HAS_FOLLOWERS", fb_followers_node)
 						tx.create(r4)
 					
 			
@@ -216,13 +243,13 @@ def main():
 				tw_followers_node = Node("Followers", name=followers)
 				tw_rts_node = Node("Rts", name=rts_avg)
 				tw_fav_node = Node("Fav", name=fav_avg)
-				rel = Relationship(game_node, "twitter_data", twitter_node)
+				rel = Relationship(game_node, "TW_DATA", twitter_node)
 				tx.create(rel)
-				r3 = Relationship(twitter_node, "has_followers", tw_followers_node)
+				r3 = Relationship(twitter_node, "HAS_FOLLOWERS", tw_followers_node)
 				tx.create(r3)
-				r4 = Relationship(twitter_node, "has_retweets", tw_rts_node)
+				r4 = Relationship(twitter_node, "HAS_RETWEETS", tw_rts_node)
 				tx.create(r4)
-				r5 = Relationship(twitter_node, "has_favorites", tw_fav_node)
+				r5 = Relationship(twitter_node, "HAS_FAVORITES", tw_fav_node)
 				tx.create(r5)
 
 	tx.commit()
